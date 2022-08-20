c021d8d6ab923cf763bb8be130f2662bceedbd96
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-08-20 22:55:54 +0200

    prettier diff

--- src/diff.sh
+++ src/diff.sh
@@ -12,4 +12,4 @@ fi
 find "$@"/* -type f  -print0 | xargs -0 -n 1 \
   yq e -i 'del(.metadata.managedFields, .metadata.annotations == with_entries(select(.key == "kubectl.kubernetes.io/last-applied-configuration")))'
 
-diff "${DIFF_ARGS[@]}" "$@"   
+diff "${DIFF_ARGS[@]}" "$@" | awk '!/^diff/ {if ($1 ~ /(---|\+\+\+)/) {print $1, $2} else {print $0}}'    
