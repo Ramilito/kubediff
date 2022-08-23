a160b2802f66fb94236738d88b86198e9de49f99
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-08-23 11:10:28 +0200

    easier to append filters, added generation filter

--- src/assets/diff.sh
+++ src/assets/diff.sh
@@ -11,6 +11,9 @@ if [[ ! $ANSIBLE_MODE = YES ]]; then
 fi
 
 find "$@"/* -type f  -print0 | xargs -0 -n 1 \
-  yq e -i 'del(.metadata.managedFields, .metadata.annotations == with_entries(select(.key == "kubectl.kubernetes.io/last-applied-configuration")))'
+  yq e -i 'del(
+    .metadata.managedFields,
+    .metadata.generation,
+    .metadata.annotations == with_entries(select(.key == "kubectl.kubernetes.io/last-applied-configuration")))'
 
 diff "${DIFF_ARGS[@]}" "$@" | awk '!/^diff/ {if ($1 ~ /(---|\+\+\+)/) {print $1, $2} else {print $0}}'    
