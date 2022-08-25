11e467119f853f526367f3aadcb4d9fbb53865dd
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-08-25 23:29:51 +0200

    cleaner diff.sh

--- src/assets/diff.sh
+++ src/assets/diff.sh
@@ -10,10 +10,10 @@ if [[ ! $ANSIBLE_MODE = YES ]]; then
   DIFF_ARGS+=("--color=always")
 fi
 
-find "$@"/* -type f  -print0 | xargs -0 -n 1 \
-  yq e -i 'del(
-    .metadata.managedFields,
-    .metadata.generation,
-    .metadata.annotations == with_entries(select(.key == "kubectl.kubernetes.io/last-applied-configuration")))'
+find "$@" -type f -exec yq e -i 'del(
+  .metadata.managedFields,
+  .metadata.generation,
+  .metadata.annotations == with_entries(select(.key == "kubectl.kubernetes.io/last-applied-configuration"))
+  )' {} \;
 
 diff "${DIFF_ARGS[@]}" "$@" | awk '!/^diff/ {if ($1 ~ /(---|\+\+\+)/) {print $1, $2} else {print $0}}'    
