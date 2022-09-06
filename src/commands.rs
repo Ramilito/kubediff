4de2af56b1cadd15e3db73cde5b08538b43b33da
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-09-07 00:23:34 +0200

    get all api-resources, then get all resources

--- src/commands.rs
+++ src/commands.rs
@@ -32,3 +32,43 @@ pub fn get_script() -> String {
     path.push("diff.sh");
     path.to_str().unwrap().to_string()
 }
+
+pub fn get_resources(resource: &str) -> String {
+    let args = vec![
+        "get",
+        "--ignore-not-found",
+        resource,
+        "-A",
+        "-o",
+        "yaml", // "jsonpath-as-json='{.items[*]['.metadata.name', '.kind']}'",
+    ];
+    let mut output = get_kubectl(args);
+    output = output.trim().to_string();
+    output
+}
+
+pub fn get_api_resources() -> Vec<String> {
+    let args = vec!["api-resources", "--verbs=list", "-o", "name"];
+    let output = get_kubectl(args);
+    let mut results: Vec<String> = output
+        .split("\n")
+        .filter(|s| !s.is_empty())
+        .map(|s| s.to_string())
+        .collect();
+
+    results.sort();
+    results
+}
+
+fn get_kubectl(args: Vec<&str>) -> String {
+    let output = Command::new("kubectl")
+        .args(args)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap()
+        .wait_with_output()
+        .unwrap();
+
+    String::from_utf8(output.stdout.to_owned()).unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ mod print;
 mod settings;
 
 use clap::Parser;
-use print::{pretty_print, pretty_print_info, pretty_print_path};
 use commands::{get_build, get_diff};
+use print::{pretty_print, pretty_print_info, pretty_print_path};
 use serde::Deserialize;
 use serde_yaml::Value;
 use settings::Settings;
@@ -23,38 +23,47 @@ fn main() -> Result<(), io::Error> {
     let args = Cli::parse();
     let mut settings = Settings::load()?;
 
-    settings.configs.env = args.env.unwrap_or_default();
-    let entry = Settings::get_service_paths(&settings)?;
+    // settings.configs.env = args.env.unwrap_or_default();
+    // let entry = Settings::get_service_paths(&settings)?;
 
-    for path in entry {
-        pretty_print_path(format!("Path: {}", path.to_string()));
+    // for path in entry {
+    //     pretty_print_path(format!("Path: {}", path.to_string()));
 
-        let build = get_build(&path);
-        for document in serde_yaml::Deserializer::from_str(build.as_str()) {
-            let v = Value::deserialize(document).unwrap();
+    //     let build = get_build(&path);
+    //     for document in serde_yaml::Deserializer::from_str(build.as_str()) {
+    //         let v = Value::deserialize(document).unwrap();
 
-            let string = serde_yaml::to_string(&v).unwrap();
-            let mut diff = get_diff();
+    //         let string = serde_yaml::to_string(&v).unwrap();
+    //         let mut diff = get_diff();
 
-            diff.stdin
-                .as_mut()
-                .unwrap()
-                .write(string.as_bytes())
-                .unwrap();
+    //         diff.stdin
+    //             .as_mut()
+    //             .unwrap()
+    //             .write(string.as_bytes())
+    //             .unwrap();
 
-            let diff = diff.wait_with_output().unwrap();
-            let string = String::from_utf8(diff.stdout.to_owned()).unwrap();
+    //         let diff = diff.wait_with_output().unwrap();
+    //         let string = String::from_utf8(diff.stdout.to_owned()).unwrap();
 
-            if string.len() > 0 {
-                pretty_print(string);
-            } else if args.verbose {
-                pretty_print_info(format!(
-                    "No changes in: {:?} {:?} {:?}\n",
-                    v["apiVersion"].as_str().unwrap(),
-                    v["kind"].as_str().unwrap(),
-                    v["metadata"]["name"].as_str().unwrap()
-                ));
-            }
+    //         if string.len() > 0 {
+    //             pretty_print(string);
+    //         } else if args.verbose {
+    //             pretty_print_info(format!(
+    //                 "No changes in: {:?} {:?} {:?}\n",
+    //                 v["apiVersion"].as_str().unwrap(),
+    //                 v["kind"].as_str().unwrap(),
+    //                 v["metadata"]["name"].as_str().unwrap()
+    //             ));
+    //         }
+    //     }
+    // }
+
+    let api_resources = commands::get_api_resources();
+    for resource in api_resources {
+        let result = commands::get_resources(&resource);
+        if result.len() > 0 {
+            let yaml = serde_yaml::to_value(&result).unwrap();
+            pretty_print(yaml.as_str().unwrap().to_string());
         }
     }
 
