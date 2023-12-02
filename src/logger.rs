24af40dad31149af68227bf4ca90430b3812c8ec
Rami Daghlawi <rami.daghlawi@gmail.com>
2023-12-02 22:41:45 +0100

    using struct for logger

--- src/logger.rs
+++ src/logger.rs
@@ -1,14 +1,27 @@
 use crate::{enums::LogLevel, print::Pretty};
 
-pub fn log(arg_log: Option<LogLevel>, config_log: LogLevel, message: String) {
-    match arg_log {
-        Some(LogLevel::Info) => Pretty::print_info(message),
-        Some(LogLevel::Warning) => println!("warning"),
-        Some(LogLevel::Error) => println!("Error"),
-        None => match config_log {
-            LogLevel::Info => Pretty::print_info(message),
-            LogLevel::Warning => println!("config: warning"),
-            LogLevel::Error => println!("config error"),
-        },
-    };
+pub struct Logger {
+    arg_log: Option<LogLevel>,
+    config_log: LogLevel,
+}
+
+impl Logger {
+    pub fn new(arg_log: Option<LogLevel>, config_log: LogLevel) -> Self {
+        Logger {
+            arg_log,
+            config_log,
+        }
+    }
+    pub fn log(&self, message: String) {
+        match &self.arg_log {
+            Some(LogLevel::Info) => Pretty::print_info(message),
+            Some(LogLevel::Warning) => println!("warning"),
+            Some(LogLevel::Error) => println!("Error"),
+            None => match &self.config_log {
+                LogLevel::Info => Pretty::print_info(message),
+                LogLevel::Warning => println!("config: warning"),
+                LogLevel::Error => println!("config error"),
+            },
+        };
+    }
 }
